d374116c2a9f8667d9f58ece66202f6e08c017bd
gonfalons <gonfalons@mykolab.com>
2019-05-20 17:17:44 +0700

    Implement get_score function for blackjack Hand

--- Blackjack.py
+++ Blackjack.py
@@ -13,13 +13,33 @@ logging.basicConfig(filename='blackjack.log', level=logging.INFO,
 DECK = Cards()
 logging.debug(DECK)
 
+
 class Hand:
     """
     functionality of the player hands 
     """
     @staticmethod
-    def get_blackjack_score(cards):
+    def get_score(cards):
         """lookup and score blackjack hands independent of instances"""
+        num_aces = 0
+        total_score = 0
+        scores = {
+            'A': 11,
+            'K': 10, 'Q': 10, 'J': 10, 'T': 10,
+            '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2
+        }
         
+        for card in cards:
+            if card[0] == 'A':
+                num_aces += 1
+            total_score += scores.get(card[0])
+
+            if total_score > 21 and num_aces:
+                total_score -= 10
+                num_aces -= 1
+
+        return total_score
 
 
+my_hand = Hand()
+print(my_hand.get_score(['Ah', 'Ad', '6s']))
--- TestBlackjack.py
+++ TestBlackjack.py
@@ -59,32 +59,31 @@ def test_hand_score_lookup():
     """test a variety of hand combinations to make sure they are scored properly"""
 
     five_pts = ['2c', '3s']
-    assert Hand.get_blackjack_score(five_pts) == 5
+    assert Hand.get_score(five_pts) == 5
 
     twelve_pts = ['As', 'Ac', 'Td']
-    assert Hand.get_blackjack_score(twelve_pts) == 12
+    assert Hand.get_score(twelve_pts) == 12
 
     thirteen_pts = ['Ad', '2s', 'Tc']
-    assert Hand.get_blackjack_score(thirteen_pts) == 13
+    assert Hand.get_score(thirteen_pts) == 13
 
     fourteen_pts = ['Ah', 'Ad', 'Ac', 'As', 'Tc']
-    assert Hand.get_blackjack_score(fourteen_pts) == 14
+    assert Hand.get_score(fourteen_pts) == 14
 
     nineteen_pts = ['Ac', '8d']
-    assert Hand.get_blackjack_score(nineteen_pts) == 19
+    assert Hand.get_score(nineteen_pts) == 19
 
     bj = ['Js', 'Ac']
-    assert Hand.get_blackjack_score(bj) == 21
+    assert Hand.get_score(bj) == 21
 
     hand_five = ['As', '2d', '3c', '4h', '5s', '6c']
-    assert Hand.get_blackjack_score(hand_five) == 21
+    assert Hand.get_score(hand_five) == 21
 
     bust_with_ace = ['Ah', '2s', 'Qh', 'Qd']
-    assert Hand.get_blackjack_score(bust_with_ace) == 23
+    assert Hand.get_score(bust_with_ace) == 23
 
     bust_no_ace = ['7s', '8h', '9d']
-    assert Hand.get_blackjack_score(bust_no_ace) == 24
-
+    assert Hand.get_score(bust_no_ace) == 24
 
 def test_hand_populates():
     pass
